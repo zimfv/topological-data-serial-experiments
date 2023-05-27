79745917c0223e9650f77f25805d7e1ddfb427e9
Fedor <zimfv@yandex.ru>
2023-05-27 03:12:47 +0300

    Create, check and start on server new lab: lab_voronoiD3_tessellation_on_torus3d.py

---
+++ lab_voronoiD3_tessellation_on_torus3d.py
@@ -0,0 +1,44 @@
+import time
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from package.filtredcells import FiltredCells
+from package.tessellations import get_latticeD3_tessellation_on_torus3d
+
+
+
+clock = time.perf_counter()
+
+sizes = np.arange(3, 65)
+experiments = 100
+
+
+print('Calculating giant cycle births for {0} experiments for {1} different 3d-torus splits by cubes.'.format(experiments, len(sizes)))
+
+bar0 = tqdm(total=len(sizes))
+bar1 = tqdm(total=experiments)
+
+filename = 'lab-results/lab_voronoiD3_tessellation_on_torus3d/size{0}_exp{1}.csv'
+
+for size in sizes:
+    bar0.set_postfix_str('size='+str(size))
+    fc = get_latticeD3_tessellation_on_torus3d(n=size)
+    filtrations = np.random.random([experiments, 4*size**3])
+    for i in range(experiments):
+        filtration = filtrations[i]
+        fc.set_filtration(filtration)
+        fc.initialize_complex()
+        #df_cycles = fc.get_giant_cycles()
+        #df_cycles.to_csv(filename.format(size, i))
+        df_info = fc.get_cycle_info()
+        df_info.to_csv(filename.format(size, i))
+        bar1.update()
+        bar1.refresh()
+    if  size != sizes[-1]:
+        bar1.reset()
+    bar0.update()
+    bar0.refresh()
+
+bar0.close()
+bar1.close()
+print('All sucessfuly calculated in {0: .4f} seconds.\n'.format(time.perf_counter() - clock))
