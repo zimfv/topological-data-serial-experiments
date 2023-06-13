4df78b399e768d27f04d546c26ed9fbc84a4dc75
Fedor <zimfv@yandex.ru>
2023-06-13 20:28:35 +0300

    Modern labs: same labs for 2-dimensional torus, but saving all cycles (not only giant)

---
+++ lab_cubical_tessellation_on_torus2d_modern_aggregating.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+sizes = np.arange(2, 129)
+experiments = np.arange(100)
+
+input_filename = 'lab-results/lab_cubical_tessellation_on_torus2d_modern/size{0}_exp{1}.csv'
+output_filename = 'lab-results/lab_cubical_tessellation_on_torus2d_modern_aggregated/size{0}.csv'
+
+bar_sizes = tqdm(total=len(sizes), desc='Sizes')
+bar_download = tqdm(total=len(experiments), desc='Downloading')
+
+for size in sizes:
+    dfs = []
+    for exp in experiments:
+        dfs.append(pd.read_csv(input_filename.format(size, exp))[['Birth', 'Death', 'Dimension']])
+        dfs[-1]['Space'] = exp
+        bar_download.update()
+    
+    df_res = pd.concat(tqdm(dfs, desc='Concat size: {0}'.format(size))) 
+    df_res.to_csv(output_filename.format(size), index=False)
+    if size != sizes[-1]:
+        bar_download.reset()
+    bar_sizes.update()
